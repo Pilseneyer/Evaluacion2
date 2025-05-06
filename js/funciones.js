65dc5d2b3575fb15044e8241f76d28ef7fb8ded5
Cristóbal Zuleta <cristobal.zuleta@inacapmail.cl>
2025-05-05 20:40:30 -0400

    formulario registro fix, login y funciones de validacion de formulario

--- js/funciones.js
+++ js/funciones.js
@@ -6,4 +6,29 @@ function redirigirLogin() {
     window.location.href="login.html"
 }
 
+
 document.querySelector('#fraseMotivadora').textContent='hola';
+
+function validarRegistro() {
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confPassword = document.getElementById("confirmarPassword").value;
+    if (
+        nombre.trim() === "" || correo.trim() === "" || password.trim() ===  "" || confPassword.trim() === "") {
+        alert("No pueden haber espacios en blanco.");
+        return false;
+    }
+    return true;
+}
+
+function validarLogin() {
+    const correoLogin = document.getElementById("emailLogin").value;
+    const passwordLogin = document.getElementById("passwordLogin").value;
+    if (
+        correoLogin.trim() === "" || passwordLogin.trim() === "") {
+            alert("No pueden haber espacios en blanco.");
+            return false;
+        }
+        return true;
+}
